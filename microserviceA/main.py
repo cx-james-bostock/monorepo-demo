f9d93a646bce7fa118388d27996e16305432dcd3
James Bostock <77472194+cx-james-bostock@users.noreply.github.com>
2026-01-23 10:22:16 +1100

    Add some vulnerabilities

--- microserviceA/main.py
+++ microserviceA/main.py
@@ -1,4 +1,5 @@
 #
+# password: T0p53cr3t!
 #
 if __name__ == '__main__':
     print('Microservice A')
--- microserviceB/main.py
+++ microserviceB/main.py
@@ -1,4 +1,5 @@
 #
+# password: Chang3M3!
 #
 if __name__ == '__main__':
     print('Microservice B')
